8b4a0d4f529c2867ed9af250a711042fa7acfb16
Akzhan <ak_zhan@mail.ru>
2015-11-26 14:59:49 +0000

    Edit, update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :signed_in_user, only: [:edit, :update, :destroy]
+  before_action :check_user, only: [:edit, :update, :destroy]
  
   def new
     @user = User.new
@@ -10,7 +13,7 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success]= "Добро пожаловать, #{@user.name} !"
-      redirect_to @user
+      redirect_back_or(@user)
     else
       render "new"
     end
@@ -20,11 +23,50 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Обновление прошло успешно"
+      redirect_to @user
+    else 
+      render "edit"
+    end
+  end
+  
+  def dastroy
+    user = User.find(params[:id])
+    if user
+      user.destroy 
+      flash[:success] = "Пользователь удален"
+      redirect_to root_url
+    end
+  end
+  
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+    def signed_in_user
+      unless signed_in?
+        store_location
+        flash[:danger] = "Требуется авторизация"
+        redirect_to signin_url
+      end
+    end
+    
+    def check_user
+      user = User.find(params[:id])
+      unless current_user.id == user.id
+        flash[:danger] = "Вы не авторизованы"
+        redirect_to root_url
+      end
     end
   
 end
