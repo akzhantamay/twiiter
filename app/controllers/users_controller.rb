5bc83d89933020709db8d669448376d19a026f29
Akzhan <ak_zhan@mail.ru>
2015-12-17 13:31:46 +0000

    11.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :signed_in_user, only: [:edit, :update, :destroy]
+  before_action :signed_in_user, only: [:edit, :update, :destroy, :following, :followers]
   before_action :check_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
  
@@ -43,13 +43,27 @@ class UsersController < ApplicationController
     end
   end
   
-  def dastroy
+  def destroy
     user = User.find(params[:id])
     if user
       user.destroy 
       flash[:success] = "Пользователь удален"
       redirect_to root_url
     end
+  end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
  root "static_pages#home"
  match "/about", to: "static_pages#about", via: :get
  match "/help", to: "static_pages#help", via: :get
- resources :users
  get "/signup", to: "users#new"
  get "/signin", to: "sessions#new"
  match "/signout", to: "sessions#destroy", via: :delete
  
- resources :users
+ resources :users do
+    member do
+      get :following, :followers
+    end
+  end
  resources :sessions, only: [:new, :create, :destroy]
  resources :microposts, only: [:create, :destroy]
+ resources :relationships, only: [:create, :destroy]
 end
